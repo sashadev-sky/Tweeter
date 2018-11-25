02eeaecfc60920e524a24899d773122b3c8602eb
sashadev-sky <sboginsky17@gmail.com>
2018-11-25 07:50:16 -0500

    add vanilla dom manipulation

---
+++ frontend/vanilla_user_search.js
@@ -0,0 +1,64 @@
+// this is just instructional. did not use or include 
+// this file anywhere in the code nor did it contribute 
+// to making the code work
+// nothing happens w/ it even though i import/export it bc
+// i never make an instance of it in our entry file, twitter.js
+
+const APIUtil = require('./api_util');
+
+const FollowToggle = require('./follow_toggle');
+
+class UsersSearch {
+  constructor(el) {
+    // el is an HTMLElement
+    this.el = el;
+    // querySelector returns the first match
+    this.input = this.el.querySelector("input[name=username]");
+    this.ul = this.el.querySelector(".users");
+
+
+    this.input.addEventListener("input", this.handleInput.bind(this));
+  }
+
+  handleInput(event) {
+    if (this.input.value === "") {
+      this.renderResults([]);
+      return;
+    }
+    APIUtil.searchUsers(this.input.value)
+      .then(users => this.renderResults(users));
+  }
+
+  renderResults(users) {
+    // setting the innerHTML to '' is equivalent to
+    // removing all the child elements
+    this.ul.innerHTML = '';
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+
+      // makes a new <a>, which is not yet attached to the DOM
+      const a = document.createElement('a');
+      a.textContent = user.username;
+      a.setAttribute("href", `/users/${user.id}`);
+
+      // makes a new <button>, which is not yet attached to the DOM
+      const followToggle = document.createElement('button');
+      new FollowToggle(followToggle, {
+        userId: user.id,
+        followState: user.followed ? "followed" : "unfollowed"
+      });
+
+      const li = document.createElement("li");
+      // adds the  <a> element to the <li>
+      li.appendChild(a);
+      // in this case add the <button> element to the <li>
+      li.appendChild(followToggle);
+
+      // and finally attaches our nicely constructed <li> to the dom
+      this.ul.appendChild(li);
+    }
+  }
+}
+
+module.exports = UsersSearch;
